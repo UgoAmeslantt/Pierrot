4a33b0ac58d95733967f2df20642a517f9c7c26f
louiservelle <louis.servelle@gmail.com>
2022-09-16 16:34:34 +0200

    handler-server

--- Back_end/main.go
+++ Back_end/main.go
@@ -1,15 +1,72 @@
 package main
 
+import(
+	"net/http"
+	"log"
+)
+
 func main(){
-	func main(){
-		http.HandleFunc("/api/")
-		http.HandleFunc("/api/login")
-		http.HandleFunc("/api/register")
-		http.HandleFunc("/api/articles")
-		http.HandleFunc("/api/articles/")
-		http.HandleFunc("/api/articles/avis/")
-		http.HandleFunc("/api/users")
-		http.HandleFunc("/api/users/")
-		http.HandleFunc("/api/panier")
-	}
+	http.HandleFunc("/api/",api)
+	http.HandleFunc("/api/login",login_handler)
+	http.HandleFunc("/api/register", register_handler)
+	http.HandleFunc("/api/articles", articles_handler)
+	http.HandleFunc("/api/articles/", artcile_handler)
+	http.HandleFunc("/api/articles/avis/", avis_handler)
+	http.HandleFunc("/api/users", users_handler)
+	http.HandleFunc("/api/users/", user_handler)
+	http.HandleFunc("/api/panier", panier_handler)
+	log.Fatal(http.ListenAndServe(":55", nil))
 }
+
+func api(w http.ResponseWriter, r *http.Request){
+
+}
+
+
+
+func login_handler(w http.ResponseWriter, r *http.Request){
+
+}
+
+
+
+func register_handler(w http.ResponseWriter, r *http.Request){
+
+}
+
+
+
+func articles_handler(w http.ResponseWriter, r *http.Request){
+
+}
+
+
+
+func artcile_handler(w http.ResponseWriter, r *http.Request){
+
+}
+
+
+
+func avis_handler(w http.ResponseWriter, r *http.Request){
+
+}
+
+
+
+func users_handler(w http.ResponseWriter, r *http.Request){
+
+}
+
+
+
+func user_handler(w http.ResponseWriter, r *http.Request){
+
+}
+
+
+func panier_handler(w http.ResponseWriter, r *http.Request){
+
+}
+
+
