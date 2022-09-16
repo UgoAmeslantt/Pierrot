fc5be9b65772309a57717fcb7f19d1fa23207321
Ugo Ameslant <ugo.ameslant@ynov.com>
2022-09-16 16:48:28 +0200

    gg

--- Back_end/main.go
+++ Back_end/main.go
@@ -1,31 +1,31 @@
 package main
 
-import(
-	"net/http"
+import (
 	"log"
+	"net/http"
 )
 
 type Users struct {
-	ID           	int    `json:"ID"`
-	Name         	string `json:"name"`
-	Password     	string `json:"password"`
-	Confirmation	string `json:"confirmation"`
-	Email       	string `json:"email"`
-	Adresse			string
-	PP           	string `json:"PP"`
+	ID           int    `json:"ID"`
+	Name         string `json:"name"`
+	Password     string `json:"password"`
+	Confirmation string `json:"confirmation"`
+	Email        string `json:"email"`
+	Adresse      string
+	PP           string `json:"PP"`
 }
 
-type Articles struct{
-	ID		int
-	Name	string
-	description string
-	prix	float64
-	picture string
+type Articles struct {
+	ID          int     `json:"ID"`
+	Name        string  `json:"Name"`
+	Description string  `json:"description"`
+	Prix        float64 `json:"prix"`
+	Picture     string  `json:"picture"`
 }
 
-func main(){
-	http.HandleFunc("/api/",api)
-	http.HandleFunc("/api/login",login_handler)
+func main() {
+	http.HandleFunc("/api/", api)
+	http.HandleFunc("/api/login", login_handler)
 	http.HandleFunc("/api/register", register_handler)
 	http.HandleFunc("/api/articles", articles_handler)
 	http.HandleFunc("/api/articles/", artcile_handler)
@@ -36,55 +36,38 @@ func main(){
 	log.Fatal(http.ListenAndServe(":55", nil))
 }
 
-func api(w http.ResponseWriter, r *http.Request){
+func api(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
-
-func login_handler(w http.ResponseWriter, r *http.Request){
+func login_handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
-
-func register_handler(w http.ResponseWriter, r *http.Request){
+func register_handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
-
-func articles_handler(w http.ResponseWriter, r *http.Request){
+func articles_handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
-
-func artcile_handler(w http.ResponseWriter, r *http.Request){
+func artcile_handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
-
-func avis_handler(w http.ResponseWriter, r *http.Request){
+func avis_handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
-
-func users_handler(w http.ResponseWriter, r *http.Request){
+func users_handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
-
-func user_handler(w http.ResponseWriter, r *http.Request){
+func user_handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
-func panier_handler(w http.ResponseWriter, r *http.Request){
+func panier_handler(w http.ResponseWriter, r *http.Request) {
 
 }
-
-
